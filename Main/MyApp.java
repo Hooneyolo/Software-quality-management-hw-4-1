698674ac070f6a4f152b53aca11db5a396a0679a
태영준 <ytae1999@naver.com>
2020-06-12 11:33:30 +0900

    main문 작성

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -2,23 +2,26 @@ package Main;
 import Calculation.Book_Rental;
 import java.util.Scanner;
 /**
- * 여기에 MyApp 클래스 설명을 작성하십시오.
+ * MyApp 클래스
  * 
-<<<<<<< HEAD
- * @author (2018315005 유제훈, 2018315033 태영준) 
-=======
- * @author (2018315005 유제훈, 2017190038 키타야마요시아키) 
->>>>>>> b24b01c7f2c52f94b834315190602e7cd9d41603
+ * @author (2018315005 유제훈, 2018315033 태영준, 2017190038 키타야마요시아키) 
  * @version (2020. 06. 12)
  */
 public class MyApp
 {
-    public static void main(String[] args){
+    public static void main(String[] args){   
         Book_Rental br = new Book_Rental();
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("나이를 입력하세요: ");
         int age = scanner.nextInt();
+        
+        System.out.print("나이를 입력하세요: ");
         int money = scanner.nextInt();
+        
+        System.out.print("나이를 입력하세요: ");
         boolean old = scanner.nextBoolean();
-        System.out.println(br.Book_Rental(age, money, old));
+               
+        System.out.println("요금은 " + br.discount_book(age, money, old) + "원 입니다.");
     }
 }
