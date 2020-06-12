b24b01c7f2c52f94b834315190602e7cd9d41603
Yoshiaki <wafflarlangel00@gmail.com>
2020-06-12 11:13:08 +0900

    commit

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -4,7 +4,7 @@ import Calculation.Book_Rental;
 /**
  * 여기에 MyApp 클래스 설명을 작성하십시오.
  * 
- * @author (2018315005 유제훈, ) 
+ * @author (2018315005 유제훈, 2017190038 키타야마요시아키) 
  * @version (2020. 06. 12)
  */
 public class MyApp 
